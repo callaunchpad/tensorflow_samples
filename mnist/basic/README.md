e26304e4860b9427b845e3f229cef8a6b8734e5e
Felix Su <felixs8696@berkeley.edu>
2017-01-22 15:25:39 -0800

    added readme to basic mnist

---
+++ mnist/basic/README.md
@@ -0,0 +1,12 @@
+# Basic MNIST
+## Description
+  - Trains neural network on mnist dataset to recognize handwritten digits
+
+## Details:
+- Layers = 3
+- Layer Sizes = [500, 500, 500]
+- Classes = 10 (digits 0-9)
+- Batch Size = 100
+- Activation Function = ReLU (Rectified Linear Unit)
+- Optimizer = AdamOptimizer (stochastic gradient descent)
+- Cost Function = softmax
--- mnist/basic/basic_mnist.py
+++ mnist/basic/basic_mnist.py
@@ -1,16 +1,3 @@
-'''
-Description: Trains neural network on mnist dataset to recognize handwritten digits
-
-Details:
-Layers = 3
-Layer Sizes = [500, 500, 500]
-Classes = 10 (digits 0-9)
-Batch Size = 100
-Activation Function = ReLU (Rectified Linear Unit)
-Optimizer = AdamOptimizer (stochastic gradient descent)
-Cost Function = softmax
-'''
-
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
